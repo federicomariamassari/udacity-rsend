72f6b9d78224e23a6107b6d2c9fc93fa7a510325
Federico Massari <federico.massari@bocconialumni.it>
2023-06-06 08:55:16 +0200

    code: Add function declaration in p5 header

--- projects/p5/catkin_ws/src/pick_objects/include/pick_objects/pick_objects.h
+++ projects/p5/catkin_ws/src/pick_objects/include/pick_objects/pick_objects.h
@@ -3,3 +3,6 @@
 #include <actionlib/client/simple_action_client.h>
 
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
+
+void navigate_to_target(MoveBaseClient &ac, move_base_msgs::MoveBaseGoal &goal, 
+                        double position_x, double position_y, double orientation_w, char* location_name);
